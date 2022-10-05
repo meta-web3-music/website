647f190e4b00e1fc782d2453c85483845170fe03
thisisommore <ommore501@gmail.com>
2022-10-05 06:02:23 +0000

    Init website

---
+++ src/routes/+page.svelte
@@ -0,0 +1,50 @@
+<header class="text-white flex px-5 py-4 items-center fixed w-full font-farro">
+	<h2 class="text-2xl">OCTAV3</h2>
+	<nav class="ml-auto flex">
+		<a href="https://octav3.gitbook.io/octav3/" target="_blank" rel="noopener noreferrer"
+			><h2 class="mr-4 hover:underline">Docs</h2></a
+		>
+		<a href="https://octav3.substack.com/" target="_blank" rel="noopener noreferrer">
+			<h2 class="mr-4 hover:underline">Blog</h2></a
+		>
+	</nav>
+</header>
+<div
+	class="landing-container text-white h-full flex items-center justify-center flex-col font-farro"
+>
+	<div class="inline-flex text-7xl">
+		<p>OC</p>
+		<iconify-icon icon="ic:baseline-music-note" class="-mx-3 -mt-0.5" />
+		<p>AV</p>
+		<p class="text-[#BF9484]">3</p>
+	</div>
+	<p class="mx-20 lg:mx-96 mt-1 text-center">
+		The decentralized music streaming platform that connects artists, listeners and advertisers.
+	</p>
+
+	<div class="flex text-[#BF9484] mt-14 absolute bottom-20">
+		<!-- TODO: Add target="_blank" rel="noopener noreferrer" -->
+		<a href="/"> <iconify-icon icon="bi:discord" class="mr-2" /></a>
+
+		<a href="https://twitter.com/octav3_music" target="_blank" rel="noopener noreferrer">
+			<iconify-icon icon="bi:twitter" /></a
+		>
+	</div>
+</div>
+
+<style>
+	.landing-container {
+		background: radial-gradient(
+					100% 225% at 100% 0%,
+					rgba(4, 36, 64, 0.8) 0%,
+					rgba(4, 36, 64, 0) 99%
+				)
+				/* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */,
+			#000000;
+	}
+	header {
+		background: radial-gradient(98% 207.98% at 85% 60%, #042440 83.24%, rgba(4, 36, 64, 0) 100%)
+				/* warning: gradient uses a rotation that is not supported by CSS and may not behave as expected */,
+			#000000;
+	}
+</style>
