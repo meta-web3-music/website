c738fb415ad19596834d1d1acdea7f42dec92845
thisisommore <ommore501@gmail.com>
2023-01-14 07:18:04 +0000

    Update text and font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
-<header class="flex px-5 py-4 items-center fixed w-full font-farro bg-white">
-	<h2 class="text-2xl">OCTAV3</h2>
-	<nav class="mx-auto flex">
+<header class="flex px-5 py-4 items-center fixed w-full font-figtree bg-white">
+	<h2 class="text-2xl font-bold">OCTAV3</h2>
+	<nav class="mx-auto flex text-[rgb(0,0,0,0.7)]">
 		<a href="https://octav3.gitbook.io/octav3/" target="_blank" rel="noopener noreferrer"
 			><h2 class="mr-4 hover:underline">Docs</h2></a
 		>
@@ -8,24 +8,24 @@
 			<h2 class="mr-4 hover:underline">Blog</h2></a
 		>
 	</nav>
-	<button class="bg-[#F3EA01] px-2 py-1 rounded-lg" on:click={()=>window.location.href="https://app.octav3.com"}>Go to app</button>
+	<button class="bg-[#F3EA01] px-8 py-2 rounded-lg font-bold" on:click={()=>window.location.href="https://app.octav3.com"}>
+		Go to app
+	</button>
 </header>
 <div
-	class="landing-container h-full flex items-center justify-center flex-col font-farro bg-[#F7F7F7]"
+	class="landing-container h-full flex items-center justify-center flex-col font-figtree bg-[#F7F7F7]"
 >
-	<div class="inline-flex text-7xl">
-		<p>OC</p>
-		<iconify-icon icon="ic:baseline-music-note" class="-mx-3 -mt-0.5" />
-		<p>AV</p>
-		<p class="text-[#BFB00D]">3</p>
+	<div class="inline-flex flex-col text-center font-bold text-7xl">
+	<p>Make Money From Your</p>
+	<p> Music NFTs</p>
 	</div>
-	<p class="mx-20 lg:mx-96 mt-1 text-center">
-		Maximize the revenue potential of your music NFTs.
+	<p class="mx-20 lg:mx-60 mt-1 text-center text-xl text-[rgb(0,0,0,0.56)]">
+		OCTAV3 is a web3 music streaming platform that connects artists, listeners and advertisers.
 	</p>
 
-	<div class="flex text-[#BFB00D] mt-14 absolute bottom-20">
+	<div class="flex text-[#BFB00D] mt-14 absolute bottom-40">
 		<!-- TODO: Add target="_blank" rel="noopener noreferrer" -->
-		<a href="/"> <iconify-icon icon="bi:discord" class="mr-2" /></a>
+		<a href="/"> <iconify-icon icon="bi:discord" class="mr-6" /></a>
 
 		<a href="https://twitter.com/octav3_music" target="_blank" rel="noopener noreferrer">
 			<iconify-icon icon="bi:twitter" /></a
